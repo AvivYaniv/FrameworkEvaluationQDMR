e71e8f3174dea52578208e26beb4d739e861f19c
AvivYaniv <avivyaniv@gmx.com>
2020-04-20 16:09:53 +0300

    Add docs

--- GraphQDMR/UnifyGraphQDMR/SemanticPreservativeVertexAction.py
+++ GraphQDMR/UnifyGraphQDMR/SemanticPreservativeVertexAction.py
@@ -1,5 +1,9 @@
 
 class SemanticPreservativeVertexAction:
+    """
+    Semantic preservative action for SINGLE vertex
+    """
+    
     @staticmethod
     def check_to_apply(vertex_qdmr):
         pass
@@ -10,6 +14,10 @@ class SemanticPreservativeVertexAction:
     
     @staticmethod
     def apply(graph_qdmr):
+        """
+        Iterate over QDMR graph vertices one by one, 
+        and apply action on vertices if needed
+        """
         for vertex_qdmr in graph_qdmr.vertices_gen():
             if SemanticPreservativeVertexAction.check_to_apply(vertex_qdmr):
                 SemanticPreservativeVertexAction.apply_on_vertex(vertex_qdmr)
