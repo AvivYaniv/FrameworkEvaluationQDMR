dcdb4da0f54939f934f43e5b0ceb80ed9f9def4c
AvivYaniv <avivyaniv@gmx.com>
2020-04-21 12:08:38 +0300

    Made unifier hooks calling more beautiful

--- GraphQDMR/UnifyGraphQDMR/UnifyGraphQDMR.py
+++ GraphQDMR/UnifyGraphQDMR/UnifyGraphQDMR.py
@@ -7,18 +7,20 @@ class UnifyGraphQDMR:
     VERTICES_ACTIONS    = []
     STRUCTURE_ACTIONS   = []
 
-    def __init__(self, hooks):
+    def __init__(self, hooks={}):
         self.hooks = hooks
+
+    def invoke_hook(self, hook_name):
+        if self.hooks.get(hook_name):
+            self.hooks[hook_name]()
 
     def convert(self, graph_qdmr):
         # TODO : Check if graph is a LIST therefore no vertex action is possible (commutative action requires more than one argument)
         self.apply_vertices_actions(graph_qdmr)
-        if self.hooks.get(UnifyGraphQDMR.HOOK_NAME_AFTER_VERTICES_ACTIONS):
-            self.hooks[UnifyGraphQDMR.HOOK_NAME_AFTER_VERTICES_ACTIONS]()
+        self.invoke_hook(UnifyGraphQDMR.HOOK_NAME_AFTER_VERTICES_ACTIONS)
         # TODO : Check if graph is a LIST therefore no structure action is possible (SOME CASES)
         self.apply_structure_actions(graph_qdmr)
-        if self.hooks.get(UnifyGraphQDMR.HOOK_NAME_AFTER_STRUCTURE_ACTIONS):
-            self.hooks[UnifyGraphQDMR.HOOK_NAME_AFTER_STRUCTURE_ACTIONS]()
+        self.invoke_hook(UnifyGraphQDMR.HOOK_NAME_AFTER_STRUCTURE_ACTIONS)
 
     def apply_vertices_actions(self, graph_qdmr):
         for vertice_action in UnifyGraphQDMR.VERTICES_ACTIONS:
