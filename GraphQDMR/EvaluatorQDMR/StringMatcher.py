b0d9d65087ee45d6b87e20410f73937458b41762
nadav352 <33449831+nadav352@users.noreply.github.com>
2020-05-06 22:24:59 +0300

    string matcher class, sotr vertex operands

---
+++ GraphQDMR/EvaluatorQDMR/StringMatcher.py
@@ -0,0 +1,21 @@
+
+from alignment.sequence import Sequence
+from alignment.vocabulary import Vocabulary
+from alignment.sequencealigner import SimpleScoring, GlobalSequenceAligner
+
+def get_sequences_match_score(seq1, seq2):
+    a = Sequence(seq1)
+    b = Sequence(seq2)
+
+    # Create a vocabulary and encode the sequences.
+    v = Vocabulary()
+    aEncoded = v.encodeSequence(a)
+    bEncoded = v.encodeSequence(b)
+
+    # Create a scoring and align the sequences using global aligner.
+    scoring = SimpleScoring(2, -1)
+    aligner = GlobalSequenceAligner(scoring, -2)
+    score, encodeds = aligner.align(aEncoded, bEncoded, backtrace=True)
+    alignment = v.decodeSequenceAlignment(encodeds[0])
+
+    return 1 - alignment.percentSimilarity()/100
--- GraphQDMR/UnifyGraphQDMR/SemanticPreservativeVertexAction.py
+++ GraphQDMR/UnifyGraphQDMR/SemanticPreservativeVertexAction.py
@@ -1,3 +1,4 @@
+from GraphQDMR import *
 
 class SemanticPreservativeVertexAction:
     """
@@ -21,3 +22,14 @@ class SemanticPreservativeVertexAction:
         for vertex_qdmr in graph_qdmr.vertices_gen():
             if SemanticPreservativeVertexAction.check_to_apply(vertex_qdmr):
                 SemanticPreservativeVertexAction.apply_on_vertex(vertex_qdmr)
+
+
+class SortVertexAttributes(SemanticPreservativeVertexAction):
+    @staticmethod
+    def check_to_apply(vertex_qdmr):
+        return True
+
+    @staticmethod
+    def apply_on_vertex(vertex_qdmr):
+        vertex_qdmr.step_desc.sort()
+        vertex_qdmr.incoming.sort(key = VertexQDMR.get_key_for_lexicographic_comparator)
