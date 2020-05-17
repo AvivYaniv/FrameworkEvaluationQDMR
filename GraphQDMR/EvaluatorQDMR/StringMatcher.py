2be2ff12ab8883444ffc8d2ac47162a2a1b9799f
nadav352 <33449831+nadav352@users.noreply.github.com>
2020-05-17 22:10:03 +0300

    StringMatcher add abilty for words sequence

--- GraphQDMR/EvaluatorQDMR/StringMatcher.py
+++ GraphQDMR/EvaluatorQDMR/StringMatcher.py
@@ -3,6 +3,17 @@ from alignment.sequence import Sequence
 from alignment.vocabulary import Vocabulary
 from alignment.sequencealigner import SimpleScoring, GlobalSequenceAligner
 
+def get_string_match_score(string1, string2, letters_sequence = True):
+
+    if letters_sequence:
+        seq1 = string1
+        seq2 = string2
+    else:
+        seq1 = string1.split(" ")
+        seq2 = string2.split(" ")
+
+    return get_sequences_match_score(seq1, seq2)
+
 def get_sequences_match_score(seq1, seq2):
     a = Sequence(seq1)
     b = Sequence(seq2)
